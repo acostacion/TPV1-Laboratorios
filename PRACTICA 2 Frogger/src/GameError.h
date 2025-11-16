378ed013f13803124f4b08aa9c12243e613593ab
Diegoucm <dimart15@ucm.es>
2025-11-16 11:56:03 +0100

    arreglado lo de GameError

--- PRACTICA 2 Frogger/src/GameError.h
+++ PRACTICA 2 Frogger/src/GameError.h
@@ -5,10 +5,7 @@
 class GameError: public std::logic_error
 {
 public:
-	GameError(const std::string& msg) : std::logic_error(msg) { message = msg; }
-	inline char const* what() const override {
-		return message.c_str();
-	}
-protected:
-	std::string message;
+	GameError(const std::string& msg) : std::logic_error(msg) {}
+
+
 };
